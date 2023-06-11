ecb6434d508b0add55ab9fe497482d12639e39dd
sennenhyorou <chenindraxr2@gmail.com>
2023-06-11 14:00:59 +0700

    Added models and define relationships

---
+++ app/Models/Article.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $table = 'articles';
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'description',
+        'status',
+        'user_id',
+        'category_id',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'article_tag', 'article_id', 'tag_id')->withTimestamps();
+    }
+
+}
